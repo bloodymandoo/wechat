7e4cfbc6085baa51a7829824a4036cbfbdfac776
bloodymandoo <1003711565@qq.com>
2018-11-05 22:48:36 +0800

    add

--- wechat/src/router/index.js
+++ wechat/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import BaseP from '@/pages/base/BaseP'
+import FindList from '@/pages/find/FindList'
+import FriendsList from '@/pages/friends/FriendsList'
+import MessageList from '@/pages/message/MessageList'
+import My from '@/pages/my/My'
 
 Vue.use(Router)
 
@@ -9,7 +13,33 @@ export default new Router({
     {
       path: '/',
       name: 'BaseP',
-      component: BaseP
+      component: BaseP,
+      children:[
+        {
+          path: '/',
+          redirect:'/MessageList'
+        },
+        {
+          path: '/MessageList',
+          name: 'MessageList',
+          component: MessageList,
+        },
+        {
+          path: '/FriendsList',
+          name: 'FriendsList',
+          component: FriendsList,
+        },
+        {
+          path: '/FindList',
+          name: 'FindList',
+          component: FindList,
+        },
+        {
+          path: '/My',
+          name: 'My',
+          component: My,
+        }
+      ]
     }
   ]
 })
