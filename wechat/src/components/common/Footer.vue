f27baa03cfefa60134c3b90e8ae7b71267c49114
shenxuan <1003711565@qq.com>
2018-11-05 19:05:13 +0800

    home

--- wechat/src/components/common/Footer.vue
+++ wechat/src/components/common/Footer.vue
@@ -1,17 +1,59 @@
 <template>
   <footer>
-
+    <nav>
+      <div v-for="item in list" :key="item.index">
+        <i :class="item.icon"></i>
+        <p>{{item.name}}</p>
+      </div>
+    </nav>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  data () {
+    return {
+      list: [
+        {
+          index: 0,
+          icon: '',
+          name: '微信'
+        },
+        {
+          index: 1,
+          icon: '',
+          name: '通讯录'
+        },
+        {
+          index: 2,
+          icon: '',
+          name: '发现'
+        },
+        {
+          index: 3,
+          icon: '',
+          name: '我'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 footer{
   @include navHei;
+  border-top:solid $footerBorColor 1px;
+  nav{
+    height:100%;
+    display:flex;
+    flex-direction: row;
+    div{
+      height:100%;
+      flex:1;
+      @include center;
+    }
+  }
 }
 </style>
--- wechat/src/components/common/Header.vue
+++ wechat/src/components/common/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <header>
-
+    <h2>微信</h2>
+    <div class="right">
+      <i class="search"></i>
+      <i class="add"></i>
+    </div>
   </header>
 </template>
 
@@ -12,6 +16,21 @@ export default {
 
 <style lang="scss" scoped>
 header{
-   @include navHei;
+  @include navHei;
+  padding:0 px2rem(20);
+  background-color:$headerBgColor;
+  color:$headerColor;
+  display: flex;
+  flex-direction: row;
+  h2{
+    height:100%;
+    @include center;
+  }
+  .right{
+    flex:1;
+    display: flex;
+    flex: row;
+    justify-content: flex-end;
+  }
 }
 </style>
